2611343cb53a89d3bcce888bcf07c6f7542b2016
Park YongJune <redplus@redplus.net>
2024-04-17 01:13:38 +0900

    정보처리기능사 실기 시험 기출 문제 - 비트 우측 시프트 연산

---
+++ src/c-language/c-language/bitwise_right_shift_operation.c
@@ -0,0 +1,14 @@
+// 정보처리기능사 실기 시험 기출 문제 - 비트 우측 시프트 연산
+// dul.me/docs/c-language/craftsman-information-processing/practical-test/past-questions/bitwise-right-shift-operation-result/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+    int num = 1640;
+    printf("%d\n", num >> 1); // 1640 / (2^1) = 820
+    printf("%d\n", num >> 2); // 1640 * (1 / 2^2) = 410
+    printf("%d\n", num >> 3); // 1640 * (1 / 2^3) = 205
+
+    return EXIT_SUCCESS;
+}
