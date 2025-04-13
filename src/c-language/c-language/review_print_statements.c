06992e715a3a9f68b0963d39c9b723f257649b6a
Park YongJune <redplus@redplus.net>
2025-04-13 15:52:04 +0900

    review_print_statements.c

---
+++ src/c-language/c-language/review_print_statements.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");  // 문자열 출력
+    printf("정수: %d\n", 123);  // 정수 출력
+    printf("실수: %.2f\n", 3.14);  // 실수 출력 (소수점 둘째 자리까지)
+    printf("문자열: %s\n", "C Programming");  // 문자열 출력
+    return 0;
+}
